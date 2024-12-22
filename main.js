98d3b7a5951c32a204677249165d74156e27a4c4
Vinicius Jorge <29614366+viniciusjps@users.noreply.github.com>
2024-12-22 05:10:19 +0000

    add parity value version

--- main.js
+++ main.js
@@ -9,14 +9,13 @@ setTimeout(() => {
 
 
 function calculate() {
-    let result = document.getElementById('result');
-    result.innerHTML = "";
+    resetViews();
 
     let bonusValue = parseFloat(document.getElementById('bonusValue').value ?? 0);
     let paidValue = parseFloat(document.getElementById('paidValue').value ?? 0);
     let resultValue = parseFloat(document.getElementById('resultValue').value ?? 0);
 
-    if (!(bonusValue + paidValue + resultValue)) {
+    if (!((bonusValue > 0) && (paidValue > 0) && (resultValue > 0))) {
         return;
     }
 
@@ -26,11 +25,55 @@ function calculate() {
 
     localStorage.setItem('results', JSON.stringify({ bonusValue, paidValue, resultValue }));
     updateView([halfValue, discount, finalValue]);
+
+    let streammerValue = bonusValue - paidValue;
+    let userValue = (paidValue / bonusValue);
+
+    const strammerFormattedValue = ` (R$ ${streammerValue}) ${((streammerValue / bonusValue) * 100)}%`;
+    const userFormattedValue = `(R$ ${paidValue}) ${(userValue * 100)}%`;
+    const parityValue = (resultValue * userValue);
+    updateParityView([strammerFormattedValue, userFormattedValue, parityValue]);
+}
+
+function resetViews() {
+    let result = document.getElementById('result');
+    result.innerHTML = "";
+}
+
+function updateParityView(items) {
+    let container = document.createElement('div');
+    let div = document.createElement('div');
+    let title = document.createElement('p');
+    title.innerHTML = 'Valor por paridade (%)';
+    title.className = 'label';
+    div.style = "display:grid; grid-gap:18px"
+    div.innerHTML = `
+        <div class="item">
+            <p>Valor do Minnie</p>
+            <p>${items[0]}</p>
+        </div>
+        <div class="item">
+            <p>Valor do Fiel</p>
+            <p>${items[1]}</p>
+        </div>
+        <div class="item">
+            <p>Valor a receber</p>
+            <p class="positive">R$ ${items[2]}</p>
+        </div>
+        `;
+    let section = document.getElementById('result');
+    container.append(title);
+    container.append(div);
+    section.append(container);
+    document.body.appendChild(section);
 }
 
 function updateView(items) {
-    let section = document.getElementById('result');
+    let container = document.createElement('div');
     let div = document.createElement('div');
+    let title = document.createElement('p');
+    title.innerHTML = 'Valores brutos';
+    title.className = 'label';
     div.style = "display:grid; grid-gap:18px"
     div.innerHTML = `
         <div class="item">
@@ -45,7 +88,10 @@ function updateView(items) {
             <p>Valor a receber</p>
             <p class="positive">R$ ${items[2]}</p>
         </div>
-        `
-    section.append(div);
-    document.appendChild(section);
+        `;
+    let section = document.getElementById('result');
+    container.append(title);
+    container.append(div);
+    section.append(container);
+    document.body.appendChild(section);
 }
